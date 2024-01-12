a0b9f66df396c98fab66a097634ba7917d086d86
Hayden <Hmfravel@gmail.com>
2024-01-12 15:45:40 -0500

    Cleaned up code. Added better solution print

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Grecian-Computer
-This is a program to solve the Grecian Computer puzzle. The Grecian Computer is a puzzle disc where there are 4 discs and a bottom layer. The bottom layer and the four discs have numbers on them which create 12 columns and 4 rows. The discs also have holes in them to allow numbers from a previous level. The objective of the puzzle is to get all 12 columns to total 42.\
+This is a program to solve the Grecian Computer puzzle. 
+The Grecian Computer is a puzzle disc where there are 4 discs and a bottom layer. 
+The bottom layer and the four discs have numbers on them which create 12 columns and 4 rows. 
+The discs also have holes in them to allow numbers from a previous level to show. 
+The objective of the puzzle is to get all 12 columns to total 42.
 # Antikythera Mechanism
-This puzzle is based off The Antikythera mechanism. This is an ancient greek hand-powered orrery that was discovered in 1901 in a shipwreck off the coast of the greek island, Antikythera. This contraption is labeled as the first Analogue computer. I gathered this information from wikipedia: https://en.wikipedia.org/wiki/Antikythera_mechanism
+This puzzle is based off The Antikythera mechanism. 
+This is an ancient greek hand-powered orrery that was discovered in 1901 in a shipwreck off the coast of the greek island, Antikythera. 
+This contraption is labeled as the first Analogue computer. 
+I gathered this information from wikipedia: https://en.wikipedia.org/wiki/Antikythera_mechanism
--- src/src/GrecianCalculator.java
+++ src/src/GrecianCalculator.java
@@ -1,31 +1,37 @@
 public class GrecianCalculator
 {
+    // The first disc is the top disc, and only extends 1 row out
     // A 0 indicates there is a hole in the disc
+    private static final int[] firstDisc = new int[]
+            {3, 0, 6, 0, 10, 0, 7, 0, 15, 0, 8, 0};
+    // The second disc is third from the bottom, and only extends 2 rows out
     // Index 0 of each array indicates the innermost row of the disc
+    private static final int[][] secondDisc = new int[][]
+            {{7, 3, 0, 6, 0, 11, 11, 6, 11, 0, 6, 17},
+            {4, 0, 7, 15, 0, 0, 14, 0, 9, 0, 12, 0}};
+    // The third disc is second from the bottom, and only extends 3 rows out
+    private static final int[][] thirdDisc = new int[][]
+            {{9,13, 9, 7, 13, 21, 17, 4, 5, 0, 7, 8},
+            {21, 6, 15, 4, 9, 18, 11, 26, 14, 1, 12, 0},
+            {5, 0, 10, 0, 8, 0, 22, 0, 16, 0, 9, 0}};
+    // The fourth disc is the bottom disc
+    private static final int[][] fourthDisc = new int[][]
+            {{7, 0, 9, 0, 7, 14, 11, 0 , 8, 0, 16, 2},
+            {9, 20, 12, 3, 6, 0, 14, 12, 3, 8, 9, 0},
+            {3, 26, 6, 0, 2, 13, 9, 0, 17, 19, 3, 12},
+            {1, 0, 9, 0, 12, 0, 6, 0, 10, 0, 10, 0}};
     private static final int[][] bottomLayer = new int[][]
             {{14, 11, 14, 11, 14, 11, 11, 14, 11, 14 ,11, 14},
             {11, 12, 13, 14, 15, 4, 5, 6, 7, 8, 9, 10},
             {14, 21, 21, 9, 9, 4, 4, 6, 6, 3, 3, 14},
             {7, 16, 8, 7, 8, 8, 3, 4, 12, 2, 5, 10}};
-    // The first disc is the bottom most disc
-    private static final int[][] firstDisc = new int[][]
-            {{7, 0, 9, 0, 7, 14, 11, 0 , 8, 0, 16, 2},
-            {9, 20, 12, 3, 6, 0, 14, 12, 3, 8, 9, 0},
-            {3, 26, 6, 0, 2, 13, 9, 0, 17, 19, 3, 12},
-            {1, 0, 9, 0, 12, 0, 6, 0, 10, 0, 10, 0}};
-    // The second disc is second from the bottom, and only extends 3 rows out
-    private static final int[][] secondDisc = new int[][]
-            {{9,13, 9, 7, 13, 21, 17, 4, 5, 0, 7, 8},
-            {21, 6, 15, 4, 9, 18, 11, 26, 14, 1, 12, 0},
-            {5, 0, 10, 0, 8, 0, 22, 0, 16, 0, 9, 0}};
-    // The third disc is third from the bottom, and only extends 2 rows out
-    private static final int[][] thirdDisc = new int[][]
-            {{7, 3, 0, 6, 0, 11, 11, 6, 11, 0, 6, 17},
-            {4, 0, 7, 15, 0, 0, 14, 0, 9, 0, 12, 0}};
-    // The fourth disc is the top disc, and only extends 1 row out
-    private static final int[] fourthDisc = new int[]
-            {3, 0, 6, 0, 10, 0, 7, 0, 15, 0, 8, 0};
+    private static final String[] printRowsStrings = new String[]
+            {"1st (Inner) Row:\t", "2nd Row:\t\t\t", "3rd Row:\t\t\t", "4th (Outer) Row:\t"};
 
+    /**
+     * Cycles through all 12 positions of each disc until the solution is found
+     * @return 4 integers of range 0-11 where index 0 is the first disc's rotation
+     */
     private static int[] bruteForce()
     {
         int[] discPos = new int[4];
@@ -38,7 +44,7 @@ public class GrecianCalculator
                 {
                     for (discPos[3] = 0; discPos[3] < 12; discPos[3]++)
                     {
-                        if (calculateColumnTotals(discPos))
+                        if (isValidSolution(discPos))
                         {
                             return discPos;
                         }
@@ -49,7 +55,11 @@ public class GrecianCalculator
         return null;
     }
 
-    private static boolean calculateColumnTotals(int[] discPos)
+    /**
+     * Returns true if all the columns in the discPos total to 42
+     * @param discPos 4 integers of range 0-11 where index 0 is the first disc's rotation
+     */
+    private static boolean isValidSolution(int[] discPos)
     {
         for (int col = 0; col < 12; col++)
         {
@@ -61,10 +71,10 @@ public class GrecianCalculator
         return true;
     }
 
-    /** Sums the 4 values for the give column
-     *
+    /**
+     * Sums the 4 values for a give column
      * @param column 0-11 for which column we're calculating
-     * @param discPos where each disc is positioned
+     * @param discPos 4 integers of range 0-11 where index 0 is the first disc's rotation
      */
     private static int sumValues(int column, int[] discPos)
     {
@@ -76,48 +86,61 @@ public class GrecianCalculator
         return total;
     }
 
-    /** Determines which number is visible for the row and column
-     *
-     * @param row 0 is calculating the outer row and 3 is calculating the inner row
-     * @param column 0-11 based on which column we're calculating
-     * @param discPos where each disc is positioned
+    /**
+     * Determines which number is visible for the row and column
+     * @param row 0 is calculating the inner row and 3 is calculating the outer row
+     * @param column 0-11 indicate which column we're calculating
+     * @param discPos 4 integers of range 0-11 where index 0 is the first disc's rotation
      */
     private static int determineValue(int row, int column, int[] discPos)
     {
-        int fourth = determineNumber(column, discPos[3]);
-        int third = determineNumber(column, discPos[2]);
-        int second = determineNumber(column, discPos[1]);
-        int first = determineNumber(column, discPos[0]);
+        int first = calculateIndex(column, discPos[0]);
+        int second = calculateIndex(column, discPos[1]);
+        int third = calculateIndex(column, discPos[2]);
+        int fourth = calculateIndex(column, discPos[3]);
 
-        if (row < 1 && fourthDisc[fourth] != 0)
-            return fourthDisc[fourth];
-        if (row < 2 && thirdDisc[row][third] != 0)
-            return thirdDisc[row][third];
-        if (row < 3 && secondDisc[row][second] != 0)
+        if (row < 1 && firstDisc[first] != 0)
+            return firstDisc[first];
+        if (row < 2 && secondDisc[row][second] != 0)
             return secondDisc[row][second];
-        if (firstDisc[row][first] != 0)
-            return firstDisc[row][first];
+        if (row < 3 && thirdDisc[row][third] != 0)
+            return thirdDisc[row][third];
+        if (fourthDisc[row][fourth] != 0)
+            return fourthDisc[row][fourth];
         return bottomLayer[row][column];
     }
 
-    private static int determineNumber(int column, int discPos)
+    /**
+     * Determines the index in the disc's array for the given column number
+     * @param column The column on the disc we're calculating for
+     * @param discPos integer for the disc's rotation of range 0-11
+     */
+    private static int calculateIndex(int column, int discPos)
     {
         return (column + discPos) % 12;
     }
 
-    private static void printSolution(int[] solution)
+    private static String solutionToString(int[] discPos)
     {
-        System.out.print("The outer row should like as follows:\n{");
-        for (int i = 0; i < 12; i++)
+        StringBuilder solution = new StringBuilder();
+        int currVal;
+
+        for (int row = 3; row >= 0; row--)
         {
-            if (firstDisc[3][solution[0]] != 0)
+            solution.append(printRowsStrings[row]).append("{ ");
+            for (int col = 0; col < 12; col++)
             {
-                System.out.print(firstDisc[3][solution[0]]);
+                currVal = determineValue(row, col, discPos);
+                if (currVal < 10)
+                    solution.append(" ");
+                solution.append(currVal);
+
+                if (col != 11)
+                    solution.append(", ");
             }
-            else
-                System.out.print(bottomLayer[i]);
-            System.out.print(", ");
+            solution.append("}\n");
         }
+        return solution.toString();
     }
 
     public static void main(String[] args)
@@ -129,11 +152,7 @@ public class GrecianCalculator
         }
         else
         {
-            System.out.println("Solution: ");
-            for (int i = 0; i < 4; i++)
-            {
-                System.out.println("\tDisc " + (i+1) + " rotation = " + solution[i]);
-            }
+            System.out.println(solutionToString(solution));
         }
     }
 }
